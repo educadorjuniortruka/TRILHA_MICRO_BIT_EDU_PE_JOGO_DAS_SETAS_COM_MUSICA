a34d80da7a8c1e569d4f3bbd5c65a814fe15dd64
educadorjuniortruka <educador.juniortruka@gmail.com>
2026-03-25 11:40:35 -0300

    Add dance game with arrow inputs and LED display
    ok

---
+++ TRILHA_MICRO_BIT_EDU_PE_JOGO_DAS_SETAS_COM_MUSICA.ts
@@ -0,0 +1,40 @@
+// JOGO DA DANÇA COM SETAS: 
+
+input.onButtonPressed(Button.A, function () {
+    for (let index = 0; index < 4; index++) {
+        basic.showLeds(`
+            . . # . .
+            . # . . .
+            # # # # #
+            . # . . .
+            . . # . .
+            `)
+        basic.pause(1000)
+        basic.showLeds(`
+        
+            . . # . .
+            . . . # .
+            # # # # #
+            . . . # .
+            . . # . .
+            `)
+        basic.pause(1000)
+        basic.showLeds(`
+            . . # . .
+            . # # # .
+            # . # . #
+            . . # . .
+            . . # . .
+            `)
+        basic.pause(1000)
+        basic.showLeds(`
+            . . # . .
+            . . # . .
+            # . # . #
+            . # # # .
+            . . # . .
+            `)
+        basic.pause(1000)
+    }
+    basic.clearScreen()
+})
